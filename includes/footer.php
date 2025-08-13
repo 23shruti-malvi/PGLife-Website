3c3ce59a757bbb134031266e8dabd2c07051d8d6
Shruti <shrutimalvi39@gmail.com>
2025-08-13 19:54:43 +0530

    PGLife

---
+++ includes/footer.php
@@ -0,0 +1,24 @@
+<div class="footer">
+        <div class="footer-container">
+            <div class="footer-cities">
+                <div class="footer-city">
+                    <a href="property_list.php?city=delhi">PG in Delhi</a>
+                </div>
+                <div class="footer-city">
+                    <a href="property_list.php?city=mumbai">PG in Mumbai</a>
+                </div>
+                <div class="footer-city">
+                    <a href="property_list.php?city=bangalore">PG in Bangalore</a>
+                </div>
+                <div class="footer-city">
+                    <a href="property_list.php?city=hyderabad">PG in Hyderabad</a>
+                </div>
+            </div>
+            <div class="footer-copyright">
+                &copy; <?php echo date("Y"); ?> Copyright PG Life
+            </div>
+        </div>
+    </div>
+
+    <script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
